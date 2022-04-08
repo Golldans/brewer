b4e6ba4ce1fbeb229beeeb90b23828f5819e3599
Golldan <cristiano.henriqueferreirabp@gmail.com>
2022-04-08 20:38:27 -0300

    Principium

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :muse,
+  ecto_repos: [Muse.Repo]
+
+# Configures the endpoint
+config :muse, MuseWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "YoF9lORZxFCRHGY3bguS6Vs6FMI9Ujuk4F19QJpyPsa5eod5j0wC6SQDflI4TdNj",
+  render_errors: [view: MuseWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Muse.PubSub,
+  live_view: [signing_salt: "uHH0CnDC"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
